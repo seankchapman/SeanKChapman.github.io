6a75f59f04f706f3f3f2e09d761af7c5adbf4d0a
schapman <seankchapman@gmail.com>
2017-01-27 14:58:13 -0800

    slowed particles

--- assets/particle/app.js
+++ assets/particle/app.js
@@ -55,7 +55,7 @@ particlesJS('particles-js',
     },
     "move": {
       "enable": true,
-      "speed": 6,
+      "speed": 2,
       "direction": "none",
       "random": false,
       "straight": false,
